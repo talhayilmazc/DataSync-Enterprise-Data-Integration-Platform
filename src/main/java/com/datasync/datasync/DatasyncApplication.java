7146ea12a13cef297c04fa043bafe75e65e67b34
Talha Yılmaz <talhayilmazc@gmail.com>
2026-05-10 11:57:24 +0300

    chore: initial project setup with Spring Boot 3, Java 17, Spring Cloud

---
+++ src/main/java/com/datasync/datasync/DatasyncApplication.java
@@ -0,0 +1,13 @@
+package com.datasync.datasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datasync/datasync/DatasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasync.datasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
